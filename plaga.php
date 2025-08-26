d2349981c0f902b229256b089e6458efd0794a19
itilnya <itilnya@oinemail.com>
2025-08-27 02:33:06 +0700

    Update plaga.php

--- plaga.php
+++ plaga.php
@@ -1,6 +1,17 @@
 <?php
+ini_set('session.gc_maxlifetime', 86400);
+session_set_cookie_params(86400);
+
 session_start();
+
 $pw = "plaga";
+
+if (isset($_GET['logout'])) {
+    session_unset();
+    session_destroy();
+    header("Location: ?");
+    exit;
+}
 
 if (!isset($_SESSION['plaga_auth'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['password'])) {
@@ -13,8 +24,8 @@ if (!isset($_SESSION['plaga_auth'])) {
     }
     ?>
     <form method="post">
-        <input type="password" name="password" placeholder="Masukin">
-        <button type="submit">aw</button>
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Login</button>
     </form>
     <?php
     exit;
